46f602e5c570fc255e94e135d407628543934bc9
nicobatungbacal <84030423+nicobatungbacal@users.noreply.github.com>
2021-05-31 20:31:43 +0800

    Updated Doctors Database

--- PatientRecordManagementSystem/database.py
+++ PatientRecordManagementSystem/database.py
@@ -10,12 +10,12 @@ def get_user(username):
     return patient
 
 doctors = {
-    10 : {"firstname":"Ernesto","lastname":"Celdran","specialty":"X-Ray","start_time":10,"end_time":17},
-    20 : {"firstname":"Maria","lastname":"Hechanova","specialty":"Blood Tests","start_time":8,"end_time":13},
-    30 : {"firstname":"Salvador","lastname":"Perez","specialty":"CT Scan","start_time":11,"end_time":19},
-    40 : {"firstname":"Teresa","lastname":"Galvez","specialty":"Ultrasound","start_time":7,"end_time":13},
-    50 : {"firstname":"Timothy","lastname":"Recto","specialty":"Pulmonology","start_time":9,"end_time":18},
-    60 : {"firstname":"Wilfredo","lastname":"Ongpin","specialty":"Molecular","start_time":12,"end_time":13}
+    10 : {"firstname":"Ernesto","lastname":"Celdran","specialty":"X-Ray","time":{"6am":0,"7am":0,"8am":0,"9am":1,"10am":1,"11am":1,"12nn":1,"1pm":1,"2pm":1,"3pm":1,"4pm":1,"5pm":0,"6pm":0}},
+    20 : {"firstname":"Maria","lastname":"Hechanova","specialty":"Blood Tests","time":{"6am":1,"7am":1,"8am":1,"9am":1,"10am":1,"11am":1,"12nn":1,"1pm":1,"2pm":0,"3pm":0,"4pm":0,"5pm":0,"6pm":0}},
+    30 : {"firstname":"Salvador","lastname":"Perez","specialty":"CT Scan","time":{"6am":0,"7am":0,"8am":1,"9am":1,"10am":1,"11am":1,"12nn":1,"1pm":1,"2pm":1,"3pm":1,"4pm":1,"5pm":1,"6pm":1}},
+    40 : {"firstname":"Teresa","lastname":"Galvez","specialty":"Ultrasound","time":{"6am":0,"7am":0,"8am":0,"9am":0,"10am":0,"11am":1,"12nn":1,"1pm":1,"2pm":1,"3pm":1,"4pm":1,"5pm":1,"6pm":1}},
+    50 : {"firstname":"Timothy","lastname":"Recto","specialty":"Pulmonology","time":{"6am":0,"7am":1,"8am":1,"9am":1,"10am":1,"11am":1,"12nn":1,"1pm":0,"2pm":0,"3pm":0,"4pm":0,"5pm":0,"6pm":0}},
+    60 : {"firstname":"Wilfredo","lastname":"Ongpin","specialty":"Molecular","time":{"6am":0,"7am":1,"8am":1,"9am":1,"10am":1,"11am":1,"12nn":1,"1pm":1,"2pm":1,"3pm":1,"4pm":1,"5pm":1,"6pm":0}}
     }
 
 def get_doctor(code):
